8260db4941833bbdb2693d2abea7453cd7fb3f9c
mraleson <markraleson@outlook.com>
2022-02-01 14:51:52 -0700

    Finish deploy command to load code onto device

--- piku/commands/deploy.py
+++ piku/commands/deploy.py
@@ -1,2 +1,26 @@
+import os
+import shutil
+from piku.core import config
+from piku.core.sync import sync
+from piku.core.backup import backup
+
+
 def deploy_command(args):
-    print('deploy')
+    # get device
+    source = config.get('system', 'source', './source')
+    device = config.get('system', 'device')
+
+    # check that device size and name are as expected to reduce chances of loading onto wrong device
+    total, used, free = shutil.disk_usage(device)
+    assert total < 3E6, 'Specified CircuitPython drive is larger than expected (~2MB).'
+    assert 'circuitpy' in device.lower(), 'Expected device to have "circuitpy" in path.'
+
+    # backup device files before deploy
+    print('Backing up device files...')
+    backup(device)
+
+    # synchronize files to device
+    print('Deploying project to device...')
+    sync(source, device, exclude=['boot_out.txt', '.*'], verbosity=1)
+
+    print('Done')
