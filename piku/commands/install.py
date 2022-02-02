55064527af502c6675bfed56f4204a5863b915e4
mraleson <markraleson@outlook.com>
2022-02-02 02:05:19 -0700

    Implement command to install dependencies from piku.toml

--- piku/commands/install.py
+++ piku/commands/install.py
@@ -1,2 +1,21 @@
+from piku.core import config, modules
+
+
 def install_command(args):
-    print('install')
+    deps = config.get('dependencies')
+    for name, version in deps.items():
+
+        # determind module source
+        if version.startswith('file:'):
+            source = version
+        else:
+            source = f'{name}@{version}'
+
+        # decode module
+        module, type, path, version = modules.decode(source)
+
+        # copy or download module
+        print(f'Acquiring {module}...')
+        if not modules.aquire(path):
+            print(f'Unable to aquire requested module: {module}')
+            return
