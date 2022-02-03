5c81bddc41ab25f2809f65bfff6130d12d93765a
mraleson <markraleson@outlook.com>
2022-02-03 16:50:33 -0700

    Print better error messages when failing to connect to serial port

--- piku/commands/serial.py
+++ piku/commands/serial.py
@@ -1,29 +1,48 @@
 from serial import Serial
+from serial.serialutil import SerialException
 from serial.tools import list_ports
 from serial.tools.miniterm import Miniterm
 from piku.core import config
 
 
 def default():
-    return list_ports.comports()[0].device
+    ports = list_ports.comports()
+    if not ports: return None
+    return ports[0].device
 
 def serial_command(args):
-    port = 115200
-    device = config.get('system', 'serial') or default()
+    baud = 115200
+    using_default = False
+    port = config.get('system', 'serial')
+    if not port:
+        using_default = True
+        port = default()
 
     # print help text
     print(r'================================================================================')
-    print(f'Connecting to serial device ${device} on port ${port}...')
+    print(f'Connecting to {"DEFAULT" if using_default else "SPECIFIED"} serial device {port} using baud {baud}...')
     print(r'  * ctrl-x: close terminal')
     print(r'  * ctrl-c: enter python repl on device')
     print(r'  * ctrl-d: exit python repl on device and reload')
     print(r'================================================================================')
 
-    # connect to serial
-    serial_instance = Serial(device, port)
+    # confirm that a port was configured or a default port was found
+    if not port:
+        print('Failed to find a configured serial port in piku.toml or load find a default serial port to use.')
+        return
+
+    # create serial port connection
+    try:
+        serial = Serial(port, baud)
+    except SerialException:
+        if using_default:
+            print(f'Failed to open the default serial port {port}')
+        else:
+            print(f'Failed to open the configured serial port {port}')
+        return
 
     # start terminal
-    miniterm = Miniterm(serial_instance, echo=False, eol='crlf')
+    miniterm = Miniterm(serial, echo=False, eol='crlf')
     miniterm.exit_character = chr(0x18)  # GS/CTRL+])
     miniterm.menu_character = chr(0x14)  # Menu: CTRL+T
     miniterm.raw = False
