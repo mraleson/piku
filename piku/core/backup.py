6393bf6c9b2dad36fad40678302d94d2f5d36aa5
mraleson <markraleson@outlook.com>
2022-02-05 23:54:32 -0700

    Make backup ignore patterns universal

--- piku/core/backup.py
+++ piku/core/backup.py
@@ -1,9 +1,19 @@
 import os
 import shutil
-from time import time
 import platform
-from shutil import ignore_patterns
+from time import time
 
+
+# perhaps im the future this is configurable along with the sync ignore/exclude patterns
+backup_ignore_patterns = shutil.ignore_patterns([
+    '.Trashes',
+    '.Trashes/*',
+    'System Volume Information',
+    'System Volume Information/*',
+    '.metadata_never_index',
+    '.fseventsd',
+    '*/._*'
+])
 
 def backup(src, dst):
     # backup drive contents
@@ -11,28 +21,13 @@ def backup(src, dst):
     timestamp = int(time() * 1000)
     backup_path = os.path.join(dst, str(timestamp))
 
-    if platform.system() == 'Darwin':
-        shutil.copytree(
-            src,
-            backup_path,
-            ignore=ignore_patterns(
-                '.Trashes',
-                '.Trashes/*',
-                'System Volume Information',
-                'System Volume Information/*',
-                '.metadata_never_index',
-                '.fseventsd',
-                '*/._*',
-            ),
-        )
-    else:
-        shutil.copytree(src, backup_path)
+    shutil.copytree(src, backup_path, ignore=backup_ignore_patterns)
 
     # remove old backups (keep the most recent 10)
     dirs = os.listdir(dst)
     dirs.sort(key=lambda x: -int(x))
     for dir in dirs[10:]:
-        if platform.system() == "Darwin":
+        if platform.system() == 'Darwin':
             shutil.rmtree(os.path.join(dst, dir), onerror=None, ignore_errors=True)
         else:
             shutil.rmtree(os.path.join(dst, dir))
